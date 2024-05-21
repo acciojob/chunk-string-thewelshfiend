3d193f012d6ba55c625215a3d5fefefdc6f7b205
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-05-21 09:28:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function stringChop(str, size) {
-  // your code here
+    // your code here
+    let arr = [];
+    if (str != ``)
+    {
+        for (let i = 0; i < str.length; i += size) {
+            arr.push(str.slice(i, i + size));
+        }
+    }
+    return arr;
 }
 
 // Do not change the code below
